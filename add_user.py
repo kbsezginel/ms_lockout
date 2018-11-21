a0a941d8039473db68f52c56fd7caa62c03168bb
kbsezginel <kbs37@pitt.edu>
2018-11-21 00:18:56 -0500

    Actually read RFID for add user script.

--- add_user.py
+++ add_user.py
@@ -3,21 +3,35 @@ Add new user to csv file.
 """
 import datetime
 from csv_helper import read_headers, append_csv
+import MFRC522
 
 
 CSV_FILE = 'ms_lockout.csv'
 
-# Read RFID card
-rfid = '111-22-33-44'
-print('RFID card read: %s' % rfid)
+RFID_READER = MFRC522.MFRC522()
 
-# Ask for user info and update CSV file
-headers = read_headers(CSV_FILE)[1:-1]
-row = [rfid]
-for h in headers:
-    value = input('Please enter %s: ' % h)
-    row.append(value)
+while True:
 
-now = datetime.datetime.now()
-row.append(now.strftime('%b-%d-%Y'))
-append_csv(CSV_FILE, row)
+# Scan for cards
+(status, TagType) = RFID_READER.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+# Get the UID of the card
+(status, uid) = RFID_READER.MFRC522_Anticoll()
+
+# If we have the UID, continue
+if status == RFID_READER.MI_OK:
+    # Read RFID card
+    uid = '-'.join([str(i) for i in uid])
+    print('RFID card read: %s' % uid)
+
+    # Ask for user info and update CSV file
+    headers = read_headers(CSV_FILE)[1:-1]
+    row = [uid]
+    for h in headers:
+        value = input('Please enter %s: ' % h)
+        row.append(value)
+
+    now = datetime.datetime.now()
+    row.append(now.strftime('%b-%d-%Y'))
+    append_csv(CSV_FILE, row)
+    break
