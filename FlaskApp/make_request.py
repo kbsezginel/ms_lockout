d6313e8d7a1057a0ecfea60ac73f3a768858cb67
kbsezginel <kbs37@pitt.edu>
2018-11-26 01:13:51 -0500

    Request IP change.

--- FlaskApp/make_request.py
+++ FlaskApp/make_request.py
@@ -4,7 +4,7 @@ Example POST request script.
 import requests
 
 
-address = 'http://0.0.0.0:5000/post'
-data = {'device': 'drill', 'status': 'on'}
+address = 'http://192.168.0.108:5000/post'
+data = {'device': 'drill', 'uid': '11-22-33-44'}
 r = requests.post(address, data=data)
 print(r.text)
